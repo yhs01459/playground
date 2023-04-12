6b0f1e7c13574d64534645ef66f2f04f61f0b142
yhs01459 <50163224+yhs01459@users.noreply.github.com>
2023-04-12 15:14:28 +0900

    add animation in loading page

--- movie-app/src/components/load.js
+++ movie-app/src/components/load.js
@@ -2,15 +2,77 @@ import styled from "styled-components";
 
 const LoadBox = styled.div`
   position: absolute;
-  top: 45%;
-  left: 45%;
+  top: 50%;
+  left: 50%;
+
+  bottom: 0;
+  &:before {
+    content: "";
+
+    width: 50px;
+    height: 5px;
+    background: #000;
+    opacity: 0.1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 50%;
+    animation: shadow 0.5s linear infinite;
+  }
+  &:after {
+    content: "";
+
+    width: 50px;
+    height: 50px;
+    background: #1a6844;
+    animation: animate 0.5s linear infinite;
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 3px;
+  }
+
+  @keyframes animate {
+    17% {
+      border-bottom-right-radius: 3px;
+    }
+    25% {
+      transform: translateY(9px) rotate(22.5deg);
+    }
+    50% {
+      transform: translateY(18px) scale(1, 0.9) rotate(45deg);
+      border-bottom-right-radius: 40px;
+    }
+    75% {
+      transform: translateY(9px) rotate(67.5deg);
+    }
+    100% {
+      transform: translateY(0) rotate(90deg);
+    }
+  }
+
+  @keyframes shadow {
+    0%,
+    100% {
+      transform: scale(1, 1);
+    }
+    50% {
+      transform: scale(1.2, 1);
+    }
+  }
 `;
 
+const Loading_letter = styled.h1`
+  position: absolute;
+  top: 55%;
+  left: 48%;
+`;
 const Load = () => {
   return (
-    <LoadBox>
-      <h1>Loading...</h1>
-    </LoadBox>
+    <div>
+      <LoadBox />
+      <Loading_letter>loading...</Loading_letter>
+    </div>
   );
 };
 
